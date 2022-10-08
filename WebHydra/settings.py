dfddea18eda0dc1fb3ef79c969c0673641ae16d8
AllanT12 <alin.tanasa@student.usv.ro>
2022-10-08 21:32:54 +0300

    Second Commit

--- WebHydra/settings.py
+++ WebHydra/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#uotsj#s*s6b-g)69nl&($r@bw7gw-9u^3*jc^j@*u#*fv3*-e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.1.241']
 
 
 # Application definition

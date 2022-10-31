94d0aa25e464ab61bc6ee6c395724c79e008b6ac
AllanT12 <50295250+AllanT12@users.noreply.github.com>
2022-10-31 16:52:47 +0200

    register

--- users/views.py
+++ users/views.py
@@ -1,9 +1,12 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from .serializer import UserSerializer
+from .models import NewUser
+from django.contrib.auth.hashers import make_password
 
 
 @api_view(['POST'])
@@ -11,9 +14,11 @@ def register(request):
     user = UserSerializer(data=request.data)
     if user.is_valid():
         user.save()
-        return Response( status=status.HTTP_201_CREATED)
+        db_password = make_password(user.data.get("password"))
+        NewUser.objects.filter(email=user.data.get("email")).update(password=db_password)
+        return Response(status=status.HTTP_201_CREATED)
     else:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+        return JsonResponse(user.errors, status=300)
 
 
 # Create your views here.
